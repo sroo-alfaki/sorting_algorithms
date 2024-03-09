0d6bd222cf880fa50aa6c0ebd184e843f2039805
saria <srooalfakiahmed@gmail.com>
2024-03-09 03:15:32 +0300

    sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,7 @@
+#include "sort.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "sort.h"
+
 /**
  * bubble_sort - function that sorts an array of integers
  * @array :array
@@ -10,20 +11,23 @@
 
 void bubble_sort(int *array, size_t size)
 {
+	int temp;
+
 	for (size_t i = 0; i < size - 1; i++)
 	{
 		for (size_t j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
+				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				for (size_t k = 0; k < size; k++)
-				{
-					printf("%d ", array[k]
-				}
-							printf("\n");
 			}
 		}
 	}
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("%d ", array[i]);
+	}
+	printf("\n");
 }
