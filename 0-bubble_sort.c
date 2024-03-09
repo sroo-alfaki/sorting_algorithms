a9310a31895cb406dac9e5d6b23ac821de9357f3
saria <srooalfakiahmed@gmail.com>
2024-03-09 03:15:36 +0300

    My

---
+++ 0-bubble_sort.c
@@ -0,0 +1,20 @@
+void bubble_sort(int *array, size_t size) 
+{
+	for (size_t i = 0; i < size - 1; i++)
+	{
+		for (size_t j = 0; j < size - i - 1; j++) 
+		{
+			if (array[j] > array[j + 1]) 
+			{
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				or (size_t k = 0; k < size; k++) 
+				{
+					printf("%d ", array[k]
+							}
+							printf("\n");
+							}
+							}
+							}
+							}
+
