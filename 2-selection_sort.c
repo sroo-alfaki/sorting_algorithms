dd60e22d7e700b514da6000704813c325f8ac35c
saria <srooalfakiahmed@gmail.com>
2024-03-11 05:26:02 +0300

    sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,22 @@
+#include "sort.h"
+/**
+ * selection_sort - Sorts a doubly linked list of integers
+ * @array: The list to be printed
+ * @size: array size
+ */
+void selection_sort(int *array, size_t size) {
+	for (size_t i = 0; i < size - 1; ++i) {
+		size_t min_index = i;
+		for (size_t j = i + 1; j < size; ++j) {
+			if (array[j] < array[min_index]) {
+				min_index = j;
+			}
+		}
+
+		int temp = array[min_index];
+		array[min_index] = array[i];
+		array[i] = temp;
+
+		print_array(array, size);
+	}
+}
