0533a5f8e2e2008c224608e026bbd1fb7862a026
saria <srooalfakiahmed@gmail.com>
2024-03-11 05:31:18 +0300

    sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -4,19 +4,25 @@
  * @array: The list to be printed
  * @size: array size
  */
-void selection_sort(int *array, size_t size) {
-	for (size_t i = 0; i < size - 1; ++i) {
+void selection_sort(int *array, size_t size)
+{
+	int temp;
+
+	for (size_t i = 0; i < size - 1; ++i)
+	{
 		size_t min_index = i;
-		for (size_t j = i + 1; j < size; ++j) {
-			if (array[j] < array[min_index]) {
+
+		for (size_t j = i + 1; j < size; ++j)
+		{
+			if (array[j] < array[min_index])
+			{
 				min_index = j;
 			}
 		}
 
-		int temp = array[min_index];
+		temp = array[min_index];
 		array[min_index] = array[i];
 		array[i] = temp;
-
 		print_array(array, size);
 	}
 }
