28a4aabd694aeacd34f753f005b70d2933b8c6c7
saria <srooalfakiahmed@gmail.com>
2024-03-11 04:41:40 +0300

    sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,11 +5,11 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *current;
+	current = (*list)->next;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	listint_t *current;
-
-	current = (*list)->next;
 
 	while (current != NULL)
 	{
