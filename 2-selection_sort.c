2a33b24396eb8fad15925dd02e6831f1a9c748a4
saria <srooalfakiahmed@gmail.com>
2024-03-11 22:39:40 +0300

    c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,31 +1,42 @@
 #include "sort.h"
+
 /**
- * selection_sort - Sorts a doubly linked list of integers
- * @array: The list to be printed
- * @size: array size
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - Sort an array of integers in ascending order
+ * @array: An array of integers.
+ * @size: The size of the array.
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, s;
-	int temp;
+	int *min;
+	size_t i, j;
 
-	for (i = 0; i < size - 1; ++i)
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
 	{
-		s = i;
-		for (j = i + 1; j < size; ++j)
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
+
+		if ((array + i) != min)
 		{
-			if (array[j] < array[s])
-			{
-				s = j;
-			}
+			swap_ints(array + i, min);
+			print_array(array, size);
 		}
-			if (array[i] > array[s])
-			{
-				temp = array[i];
-				array[i] = array[s];
-				array[s] = temp;
-				print_array(array, size);
-			}
 	}
 }
-
