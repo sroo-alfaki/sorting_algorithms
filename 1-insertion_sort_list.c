3d1687630edd49b516ce93f0e2437639a2629099
saria <srooalfakiahmed@gmail.com>
2024-03-11 04:57:29 +0300

    sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,15 +6,16 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 	current = (*list)->next;
 	while (current != NULL)
 	{
 		listint_t *temp = current;
+
 		while (temp->prev != NULL && temp->n < temp->prev->n)
 		{
-			/* Swap the nodes */
 			temp->prev->next = temp->next;
 			if (temp->next != NULL)
 				temp->next->prev = temp->prev;
